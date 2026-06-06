8398a3a3eb524f276b98ae628355f400f7f151b3
Ananya Visen <ananyavisen@gmail.com>
2026-06-06 13:36:34 +0530

    updates

--- todo/settings.py
+++ todo/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = "todo.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "DB_NAME",
+        "USER": "DB_USER",
+        "PASSWORD": "DB_Password",
+        "HOST": "DB_HOST"
     }
 }
 
