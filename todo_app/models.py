a6bd496df604b73d71457f7792318bd6313b831c
Ananya Visen <ananyavisen@gmail.com>
2026-06-11 18:30:31 +0530

    Calender feature add

--- todo_app/models.py
+++ todo_app/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Todo(models.Model):
     content = models.TextField()
     completion = models.BooleanField(default = False)
+    created_at = models.DateTimeField(auto_now_add=True)
--- todo_app/views.py
+++ todo_app/views.py
@@ -5,12 +5,23 @@ from .models import Todo
 # Create your views here.
 
 def list_todo_items(request):
-    todos = Todo.objects.all()
     count = Todo.objects.count()
+    filter = request.GET.get("filter")
+    selected_date = request.GET.get("date")
+    if(filter == "done"):
+        todos = Todo.objects.filter(completion = True)
+    elif(filter == "pending"):
+        todos = Todo.objects.filter(completion = False)
+    else:
+        todos = Todo.objects.all()
+    
+    todos = todos.filter(created_at__date = selected_date)
+    
     return render(request, "todo_app/home.html" , {"todos": todos, "count": count})
 
 def showContent(request : HttpRequest):
     content = request.POST['content'].strip()
+    filter = request.GET.get("filter")
     if(content == "" ):
         return redirect("/")
     todo = Todo(content = content)
